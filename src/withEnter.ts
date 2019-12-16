45917220328101e75cf1a1f9065cfa9ea6cdcf61
Geovanni Perez <1775792+geoperez@users.noreply.github.com>
2019-12-16 09:34:59 -0600

    Update withEnter.ts

--- src/withEnter.ts
+++ src/withEnter.ts
@@ -4,7 +4,7 @@ export const onEnterKey = (callback: () => void) => (ev: { keyCode: number }) =>
     }
 }
 
-export const asyncOnEnterKey = (callback: () => void) => async (ev: { keyCode: number }) => {
+export const onEnterKey = (callback: () => void) => async (ev: { keyCode: number }) => {
     if (ev.keyCode === 13) {
         await callback();
     }
