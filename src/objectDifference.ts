6e87104d07f75ca545e368d61233ce331b3314d7
Leonardi Pérez <marcolpr98@gmail.com>
2020-01-14 11:58:25 -0600

    Refactor objectDifference

--- src/objectDifference.ts
+++ src/objectDifference.ts
@@ -1,21 +1,16 @@
-export const objectDifference: {} = (obj1: {}, obj2: {}) => {
+export const objectDifference = (obj1: {}, obj2: {}): {} => {
     if (!obj2 || Object.prototype.toString.call(obj2) !== '[object Object]') {
         return obj1;
     }
     const diffs = {};
-    let prop: string;
 
-    const compare = (item1: {}, item2: {}): void => {
+    const compare = (item1: {}, item2: {}, prop: string): void => {
         if (item1 !== item2) {
             diffs[prop] = { prev: item1, new: item2, type: prop };
         }
     };
 
-    for (prop in obj1) {
-        if (obj1.hasOwnProperty(prop)) {
-            compare(obj1[prop], obj2[prop]);
-        }
-    }
+    Object.keys(obj1).map(prop => compare(obj1[prop], obj2[prop], prop));
 
     return diffs;
 };
