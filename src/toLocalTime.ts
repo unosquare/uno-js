5743b2c24ffa8f14d0c26a9fa5b3eeb9cf3b90f9
Leonardi Pérez <marcolpr98@gmail.com>
2020-01-15 11:37:32 -0600

    Issue29 force format (#31)
    * Update index.ts
    
    * patch version Fixed toDate
    
    * Update deps
    
    Co-authored-by: Geovanni Perez <1775792+geoperez@users.noreply.github.com>

--- src/toLocalTime.ts
+++
@@ -1,5 +0,0 @@
-export const toLocalTime = (obj: {}): void => {
-    Object.keys(obj).map(prop => {
-        if (obj[prop] instanceof Date) obj[prop] = new Date(obj[prop].toLocaleString());
-    });
-};
