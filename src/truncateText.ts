ad5990fcf2a6aa420917015646a9f21580d59696
Leonardi Pérez <marcolpr98@gmail.com>
2020-01-17 23:39:54 -0600

    What am i doing

--- src/truncateText.ts
+++ src/truncateText.ts
@@ -1,12 +1,10 @@
 export const truncateText = (text: string, length: number): string => {
     if (text && text.length > length) {
         const newString = text.substr(0, length);
-        const indexLastSpace = Array.from(newString)
-            .reverse()
-            .indexOf(' ');
+        const indexLastSpace = newString.lastIndexOf(' ');
 
         if (indexLastSpace > -1) {
-            return `${text.substr(0, (indexLastSpace - newString.length) * -1).trimEnd()}...`;
+            return `${text.substr(0, indexLastSpace).trimEnd()}...`;
         } else {
             return `${newString}...`;
         }
