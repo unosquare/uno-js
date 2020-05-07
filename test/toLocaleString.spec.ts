4640a8f6d1bc30e554085f38ce467f605b162d85
Erick Garcia <erick.garcia@unosquare.com>
2020-05-07 17:24:34 -0500

    toLocaleString tested

---
+++ test/toLocaleString.spec.ts
@@ -0,0 +1,16 @@
+import { toLocaleString } from '../src/toLocaleString';
+
+const stringDate = '2020-01-21T06:00:00';
+const locales = 'en-us';
+
+describe('toDate', () => {
+    it('Should return: January 21, 2020', () => {
+        const date = toLocaleString(stringDate, locales);
+        expect(date).toBe('January 21, 2020');
+    });
+
+    it('Should return error', () => {
+        const date = toLocaleString('aa****');
+        expect(date).toBe('');
+    });
+});
