5cdc4243af30a904bd6f21e290797b6231f016a9
Erick Garcia <erick.garcia@unosquare.com>
2020-05-08 16:49:03 -0500

    finished

---
+++ test/withEnter.spec.ts
@@ -0,0 +1,28 @@
+import { onEnterKey } from '../src/withEnter';
+import { asyncOnEnterKey } from '../src/withEnter';
+
+describe('onEnterKey', () => {
+    it('Should execute callback', () => {
+        let x = 0;
+        const callback = () => {
+            x++;
+        };
+        onEnterKey(callback)({keyCode: 0});
+        expect(x).toBe(0);
+        onEnterKey(callback)({keyCode: 13});
+        expect(x).toBe(1);
+    });
+});
+
+describe('asyncOnEnterKey', () => {
+    it('Should execute callback', () => {
+        let x = 0;
+        const callback = () => {
+            x++;
+        };
+        asyncOnEnterKey(callback)({keyCode: 0});
+        expect(x).toBe(0);
+        asyncOnEnterKey(callback)({keyCode: 13});
+        expect(x).toBe(1);
+    });
+});
