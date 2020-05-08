f7b430bc94715f804e1457f35ab8b582c9c498fc
Erick Garcia <erick.garcia@unosquare.com>
2020-05-08 13:58:56 -0500

    started

--- test/parseISO.spec.ts
+++ test/parseISO.spec.ts
@@ -1,5 +1,5 @@
 import { parseISO } from '../src/parseISO';
-
+//Started
 describe('parseISO', () => {
     it('should return valid date', () => expect(parseISO('2010-01-01T06:00:00.000Z') instanceof Date).toBe(true));
 });
