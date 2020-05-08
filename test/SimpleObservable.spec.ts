4f5a97970483b3af25b6534ee8a0b599f0e980bc
Erick Garcia <erick.garcia@unosquare.com>
2020-05-08 11:56:59 -0500

    done

--- test/SimpleObservable.spec.ts
+++ test/SimpleObservable.spec.ts
@@ -4,18 +4,18 @@ describe('SimpleObservable', () => {
     const observable = new SimpleObservable();
     let x = 5;
     
-    const fm = observable.subscribe((status)=>{
+    const fm = observable.subscribe((status) => {
         if(status == 'Count') x++;
     });
 
-    observable.inform('Count');
     it('x shoud be: 6', () => {
+        observable.inform('Count');
         expect(x).toBe(6);
     });
 
-    fm();
-    observable.inform('Count');
     it('x shoud be: 6', () => {
+        fm();
+        observable.inform('Count');
         expect(x).toBe(6);
     });
 });
