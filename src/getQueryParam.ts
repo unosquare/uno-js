7ba92a8202eb7c13eb0206a803a0c7404e671719
Leonardi Pérez <marcolpr98@gmail.com>
2020-07-21 08:16:02 -0500

    Updated GetQueryParam logic

--- src/getQueryParam.ts
+++ src/getQueryParam.ts
@@ -3,9 +3,13 @@ export const getQueryParam = (param: string, url: string): string | number | nul
     const match = url.match(regex);
     if (match) {
         const hasMoreValues = url.indexOf('&');
+        const hasAnchor = url.lastIndexOf('#');
         const initial = match.index + match[0].length;
-        if (hasMoreValues >= 0) {
+        if (hasMoreValues >= initial) {
             return url.substr(initial, hasMoreValues - initial);
+        }
+        if (hasAnchor >= initial) {
+            return url.substr(initial, hasAnchor - initial);
         }
         return url.substr(initial);
     }
--- test/getQueryParam.spec.ts
+++ test/getQueryParam.spec.ts
@@ -3,6 +3,8 @@ import { getQueryParam } from '../src/getQueryParam';
 describe('getQueryParam', () => {
     const singleUrl = 'https://github.com/unosquare/uno-js?id=3';
     const multipleUrl = 'https://github.com/unosquare/uno-js?id=3&user=MarcoLPR';
+    const anchorUrl = 'https://github.com/unosquare/uno-js?id=3#Documents';
+
     it('should return id', () => {
         const id = getQueryParam('id', singleUrl);
         expect(id).toBe('3');
@@ -11,8 +13,16 @@ describe('getQueryParam', () => {
         const id = getQueryParam('id', multipleUrl);
         expect(id).toBe('3');
     });
+    it('should return user on multiple params', () => {
+        const id = getQueryParam('user', multipleUrl);
+        expect(id).toBe('MarcoLPR');
+    });
     it('should return null', () => {
         const id = getQueryParam('user', singleUrl);
         expect(id).toBe(null);
+    });
+    it('should return id without anchor', () => {
+        const id = getQueryParam('id', anchorUrl);
+        expect(id).toBe('3');
     });
 });
