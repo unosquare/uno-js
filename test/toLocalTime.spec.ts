01a079207d732dab2e51c575a5aef28aa84d2e14
Leonardi Pérez <marcolpr98@gmail.com>
2020-10-06 10:13:11 -0500

    Added validation in toLocalTime

--- test/toLocalTime.spec.ts
+++ test/toLocalTime.spec.ts
@@ -4,7 +4,12 @@ describe('toDate', () => {
     it('string should return true if Date', () => {
         const date = toLocalTime('2020-01-21T06:00:00');
         expect(date.constructor === Date && date.getFullYear() === 2020).toBe(true);
-    });    
+    });
+
+    it('string should return true if Date', () => {
+        const date = toLocalTime('2020-01-01T08:00:00Z');
+        expect(date.constructor === Date && date.getFullYear() === 2020).toBe(true);
+    });
 
     it('Date should return true if date works', () => {
         const date = toLocalTime(new Date(2020, 1, 21, 6, 0, 0));
