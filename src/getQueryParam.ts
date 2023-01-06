d074c2b80069cf7d9df7c0cead78a895d673ca7a
Daniel Michel <daniel.michel@unosquare.com>
2023-01-06 14:12:10 -0600

    fix test

--- src/getQueryParam.ts
+++ src/getQueryParam.ts
@@ -9,13 +9,13 @@ const getQueryParam = (param: string, url: string): string | number | null => {
     const restUrl = url.substring(match.index + match[0].length);
     const hasMoreValues = restUrl.indexOf('&');
 
-    if (hasMoreValues) {
+    if (hasMoreValues !== -1) {
         return restUrl.substring(0, hasMoreValues);
     }
 
     const hasAnchor = restUrl.lastIndexOf('#');
 
-    return hasAnchor ? restUrl.substring(0, hasAnchor) : restUrl;
+    return hasAnchor !== -1 ? restUrl.substring(0, hasAnchor) : restUrl;
 };
 
 export default getQueryParam;
