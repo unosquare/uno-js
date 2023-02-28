cef7a008e3c742d67ef21cbbc8efb3b97a303b62
Oscar Gonzalez <oscarf.gonzalezq@gmail.com>
2023-02-27 23:20:19 -0400

    Adjust formatting for mixed entries

--- src/trimText.ts
+++ src/trimText.ts
@@ -1,3 +1,9 @@
-const trimText = (text: string) => text.toString().replace(/\s+/g, ' ');
+const trimText = (text: string | Array<string>) =>
+    text.constructor === Array
+        ? text
+              .map((x: string, i: number) => (i === 0 ? x : ` ${x}`))
+              .toString()
+              .replace(/\s+/g, ' ')
+        : text.toString().replace(/\s+/g, ' ');
 
 export default trimText;
