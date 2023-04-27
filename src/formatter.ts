b107e3ab5549a197423eae5a9b0d58364db78aff
Antonio Gonzalez <antonio.ggovea@hotmail.com>
2023-04-27 16:47:15 -0600

    Add toFixed to decimal percentage

--- src/formatter.ts
+++ src/formatter.ts
@@ -32,7 +32,7 @@ export const formatter = (data: string | number, format: FormatTypes): string =>
         case FormatTypes.PERCENTAGE:
             return `${Math.round(parseFloat(stringData))}%`;
         case FormatTypes.DECIMAL_PERCENTAGE:
-            return `${truncate(parseFloat(stringData))}%`;
+            return `${truncate(parseFloat(stringData)).toFixed(2)}%`;
         case FormatTypes.NUMBER:
             return `${parseInt(stringData, 10)}`;
         case FormatTypes.DATE:
